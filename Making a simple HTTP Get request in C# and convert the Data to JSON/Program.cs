3da87a40b0100dce4aa4e931c0b93d3efc786c95
Ayush Gupta <67874887+Ayu449@users.noreply.github.com>
2021-06-25 00:02:55 +0530

    Add files via upload

---
+++ Making a simple HTTP Get request in C# and convert the Data to JSON/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            dynamic response = await Program.makeRequest("https://data-asg.goldprice.org/dbXRates/USD");
+            Console.WriteLine(response);
+            Console.ReadLine();
+        }
+
+        public static async Task<dynamic> makeRequest(string url)
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync(url);
+            dynamic json = JsonConvert.DeserializeObject(result);
+            return json;
+
+        }
+
+    }
+}
